c4018f86caa8fb0262905a9bf6adc8319948abb2
Carlos-Cardenas-Bravo <cardenasbravo@gmail.com>
2024-10-23 23:46:02 -0300

    se corrigió el boton de eliminar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # rubocop:disable Layout/SpaceInsideArrayLiteralBrackets
 Rails.application.routes.draw do
+  root "photos#index"
   devise_for :users
-
   resources :users, only: [:index]
 
   resources :photos do
@@ -9,13 +9,7 @@ Rails.application.routes.draw do
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  root "photos#index"
 end
-
 # rubocop:enable Layout/SpaceInsideArrayLiteralBrackets
